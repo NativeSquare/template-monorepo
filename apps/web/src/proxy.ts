bab2f507e0ccf2bf31c35d26ebbd41ffc755753c
Maxime-NS <maxime.gey@nativesquare.fr>
2026-02-05 23:09:52 +0100

    feat: web auth

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -4,15 +4,22 @@ import {
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
 
-const isAuthRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
-const isProtectedRoute = createRouteMatcher(["/app/dashboard"]); // TODO : Redirect to /app by default
+const isAuthRoute = createRouteMatcher([
+  "/login",
+  "/signup",
+  "/forgot-password",
+  "/reset-password",
+  "/verify-email",
+  "/otp",
+]);
+const isProtectedRoute = createRouteMatcher(["/dashboard"]); // TODO : Redirect to /app by default
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
   if (isAuthRoute(request) && (await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/app/dashboard");
+    return nextjsMiddlewareRedirect(request, "/dashboard");
   }
   if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    return nextjsMiddlewareRedirect(request, "/sign-in");
+    return nextjsMiddlewareRedirect(request, "/login");
   }
 });
 
