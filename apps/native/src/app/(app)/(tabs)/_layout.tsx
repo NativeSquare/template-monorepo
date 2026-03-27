f4344d11ad47add862d092b3b91b8cd5f9815f4e
Maxime-NS <maxime.gey@nativesquare.fr>
2026-03-27 13:49:28 +0100

    fix: typecheck errors

--- apps/native/src/app/(app)/(tabs)/_layout.tsx
+++ apps/native/src/app/(app)/(tabs)/_layout.tsx
@@ -1,19 +1,15 @@
-import {
-  Label,
-  Icon as NativeIcon,
-  NativeTabs,
-} from "expo-router/unstable-native-tabs";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function TabsLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <Label>Home</Label>
-        <NativeIcon sf={"house.fill"} drawable={"ic_menu_mylocation"} />
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf={"house.fill"} drawable={"ic_menu_mylocation"} />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="account">
-        <Label>Account</Label>
-        <NativeIcon sf={"person.fill"} drawable={"ic_menu_mylocation"} />
+        <NativeTabs.Trigger.Label>Account</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf={"person.fill"} drawable={"ic_menu_mylocation"} />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
